69872223e0f59521cb742cc51f91c7f0ea1c37d0
Pierre Paquin <p.paquin1016@laposte.net>
2022-06-21 21:18:03 +0200

    wip: creation startup system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Foyer MEP horror".to_string(),
+            width: 600.0,
+            height: 600.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // add rectangle
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(150., 150.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
