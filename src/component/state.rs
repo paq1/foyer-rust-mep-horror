d6e0501a8df33f049a971ed63d3369182fa666ae
paq1 <p.paquin1016@laposte.net>
2022-06-27 15:53:00 +0200

    feat: changemen t texts

--- src/component/state.rs
+++ src/component/state.rs
@@ -3,5 +3,9 @@ use bevy::prelude::Component;
 #[derive(Component)]
 pub struct InGameComponent;
 
+
+#[derive(Component)]
+pub struct InMenuComponent;
+
 #[derive(Component)]
 pub struct InEndgameComponent;
--- src/plugins/menu_plugin.rs
+++ src/plugins/menu_plugin.rs
@@ -2,8 +2,9 @@ use bevy::prelude::*;
 
 use crate::{AppState, POLICE};
 use crate::component::{
-    menu::TextMenu
+    state::InMenuComponent
 };
+use crate::resources::{WinSize};
 
 pub struct MenuPlugin;
 
@@ -27,16 +28,41 @@ impl Plugin for MenuPlugin {
 
 fn write_menu_system(
     asset_server: Res<AssetServer>,
-    mut commands: Commands
+    mut commands: Commands,
+    win_size: Res<WinSize>
 ) {
     // on ajoute le texte du score
+
+
     commands
         .spawn_bundle(Text2dBundle {
             text: Text {
                 sections: vec![TextSection {
-                    value: "press [space] to start".to_string(),
+                    value: "Foyer MEP horror".to_string(),
                     style: TextStyle {
                         color: Color::RED,
+                        font: asset_server.load(POLICE),
+                        font_size: 36.
+                    }
+                }],
+                alignment: TextAlignment {
+                    horizontal: HorizontalAlign::Center,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(0., win_size.h / 2., 1.),
+            ..Default::default()
+        })
+        .insert(InMenuComponent);
+
+    commands
+        .spawn_bundle(Text2dBundle {
+            text: Text {
+                sections: vec![TextSection {
+                    value: "Press [X] pour fix un bug lorsqu'un\nfichier \"fixme\" apparait".to_string(),
+                    style: TextStyle {
+                        color: Color::ORANGE,
                         font: asset_server.load(POLICE),
                         font_size: 36.
                     }
@@ -52,16 +78,41 @@ fn write_menu_system(
             // transform: Transform::from_xyz(pos_score.0, pos_score.1, 1.),
             ..Default::default()
         })
-        .insert(TextMenu);
+        .insert(InMenuComponent);
+
+    commands
+        .spawn_bundle(Text2dBundle {
+            text: Text {
+                sections: vec![TextSection {
+                    value: "Press [Espace] pour jouer".to_string(),
+                    style: TextStyle {
+                        color: Color::YELLOW,
+                        font: asset_server.load(POLICE),
+                        font_size: 36.
+                    }
+                }],
+                
+                alignment: TextAlignment {
+                    //vertical: VerticalAlign::Center,
+                    horizontal: HorizontalAlign::Center,
+                    ..Default::default()
+                },
+                
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(0., -win_size.h / 2. + 36., 1.),
+            ..Default::default()
+        })
+        .insert(InMenuComponent);
 }
 
 
 fn on_exit_menu_sytem(
     mut commands: Commands,
-    query: Query<Entity, With<TextMenu>>
+    query: Query<Entity, With<InMenuComponent>>
 ) {
-    let entity = query.get_single().unwrap();
-    commands.entity(entity).despawn();
+    query.iter()
+        .for_each(|entity| commands.entity(entity).despawn());
 }
 
 fn from_menu_to_game_system(
@@ -71,4 +122,12 @@ fn from_menu_to_game_system(
     if kb.just_pressed(KeyCode::Space) {
         app_state.set(AppState::InGame).unwrap();
     }
+}
+
+fn text_menu() -> String {
+    vec![
+        "Foyer MEP horror",
+        "fixer le plus de bugs avant la MEP",
+        "appuyer [espace] pour jouer"
+    ].join("\n\n\n")
 }
